103ae92a8c4364386d8224dd37f2ec3fff4baa34
Swamy Vaditya <swamyvaditya@gmail.com>
2025-07-25 18:42:25 +0530

    Changes made for ci/cd

--- sample-app/server.js
+++ sample-app/server.js
@@ -1,5 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js App!");
+});
+
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello from Sample App v1!'));
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
