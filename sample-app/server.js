5c5ffdecbafbb5646b3edd8112998ec429a63488
Swamy Vaditya <swamyvaditya@gmail.com>
2025-07-25 17:54:55 +0530

    Updated for express.js backend and new argocd app

---
+++ sample-app/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello from Sample App v1!'));
+app.listen(port, () => console.log(`App listening on port ${port}`));
