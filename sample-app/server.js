d4645d584308fc955e8af7a2f81696651959972f
Swamy Vaditya <swamyvaditya@gmail.com>
2025-07-25 21:30:14 +0530

    Testing Argo CD Sync

--- sample-app/server.js
+++ sample-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Latest updates!!! from Node.js App!!!");
+  res.send("Congratulations!!! Argo CD Worked!!!");
 });
 
 const port = process.env.PORT || 3000;
