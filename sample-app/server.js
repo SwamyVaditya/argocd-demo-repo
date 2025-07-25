bdea70929466dcb66dfa52c9ee591aabeed6c913
Swamy Vaditya <swamyvaditya@gmail.com>
2025-07-25 20:06:34 +0530

    updated server.js

--- sample-app/server.js
+++ sample-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello again .. Updates from Node.js App!");
+  res.send("Updates from Node.js App!");
 });
 
 const port = process.env.PORT || 3000;
